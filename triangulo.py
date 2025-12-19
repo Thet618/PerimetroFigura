757a351149faa5ab30e18a39056ea815ad97c07b
Thet618 <danielsummon258@gamil.com>
2025-12-19 12:34:32 -0600

    Perimetro del triangulo

---
+++ triangulo.py
@@ -0,0 +1,9 @@
+# Calcular el perímetro de un triángulo
+
+lado1 = float(input("Ingresa el primer lado del triángulo: "))
+lado2 = float(input("Ingresa el segundo lado del triángulo: "))
+lado3 = float(input("Ingresa el tercer lado del triángulo: "))
+
+perimetro = lado1 + lado2 + lado3
+
+print(f"El perímetro del triángulo es: {perimetro}")
